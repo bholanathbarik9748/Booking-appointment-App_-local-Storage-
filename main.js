3c33e31151e4c8aa74bac583e7dbf1d4e4acc868
Bholanath <bholanathbarik9748@gmail.com>
2022-11-22 16:53:20 +0530

    version 1

---
+++ main.js
@@ -0,0 +1,13 @@
+function sConsole(event) {
+    event.preventDefault();
+    var name = document.getElementById("data").value;
+    var phone = document.getElementById("phone").value;
+    var date = document.getElementById("date").value;
+    console.log(name);
+    console.log(phone);
+    console.log(date);
+
+    localStorage.setItem('Name',name);
+    localStorage.setItem('Phone no',phone);
+    localStorage.setItem('Date',date);
+  }
