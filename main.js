632399206fadd107c7b4f83ec1baf2fc8f1a6e40
Bholanath <bholanathbarik9748@gmail.com>
2022-11-22 19:39:38 +0530

    version 3

--- main.js
+++ main.js
@@ -6,12 +6,23 @@ function sConsole(event) {
     console.log(name);
     console.log(phone);
     console.log(date);
-    
-    const myData ={
-        Full_name : name,
-        phone_no : phone,
-        current_Data : date
+
+    const myData = {
+        Full_name: name,
+        phone_no: phone,
+        current_Data: date
     }
 
-    window.localStorage.setItem('myObject',JSON.stringify(myData));
-  }
+    window.localStorage.setItem(myData.phone_no, JSON.stringify(myData));
+    show_User(myData);
+    for(let i=0; i< localStorage.length; i++) {
+        let key = localStorage.key(i);
+        alert(`${key}: ${localStorage.getItem(key)}`);
+    }
+}
+
+function show_User(user){
+    var ele = document.getElementById('List_of_user');
+    var childHtml = `<li>${user.Full_name} -- ${user.phone_no} -- ${user.current_Data}</li>`;
+    ele.innerHTML = ele.innerHTML + childHtml;
+}
